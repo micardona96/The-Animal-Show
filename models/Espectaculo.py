284af466fd586357a8f3e19e3e0ba952d0bbc1ef
micardona96 <mcardona.complex@gmail.com>
2020-05-12 07:08:48 -0500

    se agregan paramatros iniciales en el espectaculo

--- models/Espectaculo.py
+++ models/Espectaculo.py
@@ -1,4 +1,9 @@
 class Espectaculo:
-    def __init__(self, apertura=[], resto=[]):
+    def __init__(self, partes, animales, escenas, apertura=[],  resto=[]):
         self.apertura = apertura
         self.resto = resto
+
+   # cantidad de elementos del espectaculo
+        self.partes = partes        # m partes
+        self.animales = animales    # n animales
+        self.escenas = escenas      # k escenas
