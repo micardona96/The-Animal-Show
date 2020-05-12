2b05c3f7957aefc8b58f6c663be86fc7a1eef5a1
Gustavo <gustavo.pinto@correounivalle.edu.co>
2020-05-12 10:59:51 -0500

    Se implemento counting sort para ordenar las partes del espectaculo

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ animal6 = Animal('Caballo', 6)
 escena1 = Escena([animal1, animal2, animal4])
 escena2 = Escena([animal3, animal1, animal2])
 escena3 = Escena([animal5, animal1, animal6])
+escena4 = Escena([animal6, animal3, animal1])
+escena5 = Escena([animal4, animal2, animal5])
 
 parte1 = Parte([escena1, escena2, escena3])
+parte2 = Parte([escena4, escena2, escena5])
 
 print('Animales Ordenados por cada escena')
 escena1.sortN()
@@ -23,10 +26,26 @@ escena2.sortN()
 print(escena2)
 escena3.sortN()
 print(escena3)
+escena4.sortN()
+print(escena4)
+escena5.sortN()
+print(escena5)
 
 print('\n')
 
-print('Escenas ordenadas por cada parte')
+print('Escenas ordenadas por cada parte\n')
 parte1.sortN()
 
+parte2.sortN()
+
 print(parte1)
+print(parte1.getGrandezaTotal())
+
+print(parte2)
+print(parte2.getGrandezaTotal())
+
+print('Partes Ordenadas por espectaculo\n')
+espectaculo = Espectaculo(partes=[parte2, parte1])
+espectaculo.sortN()
+
+print(espectaculo)
--- models/Espectaculo.py
+++ models/Espectaculo.py
@@ -1,9 +1,32 @@
 class Espectaculo:
-    def __init__(self, partes, animales, escenas, apertura=[],  resto=[]):
+    def __init__(self, partes, animales=[], escenas=[], apertura=[]):
         self.apertura = apertura
-        self.resto = resto
 
    # cantidad de elementos del espectaculo
         self.partes = partes        # m partes
         self.animales = animales    # n animales
         self.escenas = escenas      # k escenas
+
+    
+    def sortN(self):
+        outputArray = [None]*len(self.partes)
+        countArray = [0]*45
+
+        for parte in self.partes:
+            countArray[parte.getGrandezaTotal() - 1] += 1
+
+        for i in range(1,len(countArray)):
+            countArray[i] += countArray[i-1]
+
+        for i in range(len(self.partes)-1, -1, -1):
+            outputArray[countArray[self.partes[i].getGrandezaTotal() - 1] - 1] = self.partes[i]
+        
+        self.partes = outputArray 
+
+
+    def __str__(self):
+        description = ''
+        for parte in self.partes:
+            description += str(parte) + '\n'
+        return description
+
