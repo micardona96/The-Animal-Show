bd0cc0f8b09e1897075021a2310b322cfe7104bf
micardona96 <mcardona.complex@gmail.com>
2020-05-15 01:07:56 -0500

    add space in format

--- lib/small.py
+++ lib/small.py
@@ -54,13 +54,13 @@ espectaculo = Espectaculo(
     escenas=ESCENAS,
     apertura=parteApertura)
 
+
 print('\033[94m' + '\n--- RESULTADOS ---' + '\033[0m')
 espectaculo.main(algoritmo='N')
 espectaculo.imprimirResultados()
 
 
 print('\033[94m' + '\n--- ANÁLISIS DE COMPLEJIDAD ---' + '\033[0m')
-
 resultado1 = timeit("espectaculo.main(algoritmo='N')",
                     globals=globals(), number=200)
 print('Solución O(N)  \t \t' + str(resultado1 * 1000))
